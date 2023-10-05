369955e3dbef52e6a0759500110e4acc97c27e40
Boulay Mathias <mathiasboulay@free.fr>
2023-10-05 11:39:57 +0200

    Chore: setup react native paper (#1)
    * chore: setup format lint & ui lib
    
    * chore: setup react native paper
    
    ---------
    
    Co-authored-by: Martin Moreira de Jesus <martin.moreiradj@icloud.com>

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export function HelloWorld() {
+  return (
+    <View>
+      <Text variant="displayLarge">Hello World!</Text>
+    </View>
+  );
+}
