8c0184737311f2d2f91a9d187332d5b987e926b9
Boulay Mathias <mathiasboulay@free.fr>
2023-10-05 17:07:14 +0200

    chore: setup a store with zustand + immer (#2)

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,10 +1,14 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { useStore } from 'src/store/store';
 
 export function HelloWorld() {
+  const store = useStore();
+
   return (
     <View>
-      <Text variant="displayLarge">Hello World!</Text>
+      <Text variant='displayLarge'>Hello World!</Text>
+      <Text>Have you considered calling yourself "{store.user.name}"?</Text>
     </View>
   );
 }
